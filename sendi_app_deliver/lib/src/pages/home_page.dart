6caea2d27d754971a6c82f8bf0a9fd1b8cbeb29c
Oliver Yunior Mendez <olivermendezpersonal9@gmail.com>
2021-06-07 22:22:53 -0400

    Tarjeta principal del delivery avanzada

--- sendi_app_deliver/lib/src/pages/home_page.dart
+++ sendi_app_deliver/lib/src/pages/home_page.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
         body: SafeArea(
           child: SingleChildScrollView(
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -37,8 +37,80 @@ class HomePage extends StatelessWidget {
                         "Your Account",
                         style: TextStyle(
                             fontSize: 30, fontWeight: FontWeight.w800),
-                      )
+                      ),
                     ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Info about the account",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SingleChildScrollView(
+                    padding: EdgeInsets.only(bottom: 20),
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.90,
+                          height: 200,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 20),
+                          //tarjeta
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Colors.yellow[100],
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius: 0,
+                                  offset: Offset(0, 1),
+                                  blurRadius: 0,
+                                  color: Colors.black.withOpacity(0.2))
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "DOS: 12,105.67",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.w500),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Made today',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              Expanded(
+                                  child: Container(
+                                //imagen de vehiculo aqui
+                                decoration: BoxDecoration(),
+                              )),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text("Completa mas viajes",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500)),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                children: [],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
